83a81af724f95027769468b403a9d7e9210e704b
cchrisking <christou2c@gmail.com>
2025-05-07 07:21:10 +0200

    frot end working properly

--- src/controllers/User_controller.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by christou on 4/24/25.
-//
-
-#include "User_controller.h"
---
+++ src/utils/utils.h
@@ -0,0 +1,16 @@
+//
+// Created by christou on 5/6/25.
+//
+
+#ifndef UTILS_H
+#define UTILS_H
+
+
+
+class utils {
+
+};
+
+
+
+#endif //UTILS_H
